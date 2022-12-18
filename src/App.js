cea1da3884203eb68cb2cee05bbae0cbc615361a
Abdullah-Sakib <abdullahsakib888@gmail.com>
2022-12-18 20:55:57 +0600

    Save data to database

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Toaster } from "react-hot-toast";
 import "./App.css";
 import Home from "./Components/Home";
 
@@ -5,6 +6,7 @@ function App() {
   return (
     <div className="min-h-screen flex items-center justify-center">
       <Home></Home>
+      <Toaster />
     </div>
   );
 }
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,13 +1,54 @@
 import React, { useEffect, useState } from "react";
+import { toast } from "react-hot-toast";
 
 const Home = () => {
-  const [options, setOptions] = useState([]);
+  const [sectors, setSectors] = useState([]);
   useEffect(() => {
-    fetch("http://localhost:5000/getOptions")
+    fetch("http://localhost:5000/getSectors")
       .then((res) => res.json())
-      .then((data) => setOptions(data));
+      .then((data) => setSectors(data));
   }, []);
-  console.log(options);
+
+  const handleSave = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const sector = form.sector.value;
+    const agree = form.agree.checked;
+    if (name.length === 0) {
+      toast.error("Please provide your name");
+      return;
+    } else if (sector.length === 0) {
+      toast.error("Please select the sector you are currently involved in.");
+      return;
+    } else if (agree === false) {
+      toast.error("Sorry, You must agree our terms to continue.");
+      return;
+    }
+
+    const data = {
+      name,
+      sector,
+      agree,
+    };
+
+    fetch("http://localhost:5000/saveData", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => {
+        if (res.status === 409) {
+          toast.error("Please try another name. This name has already taken");
+          return;
+        }
+        return res.json();
+      })
+      .then((data) => console.log(data));
+  };
+
   return (
     <div>
       <section className="max-w-4xl p-6 mx-auto bg-white rounded-md shadow-xl dark:bg-gray-800">
@@ -16,17 +57,18 @@ const Home = () => {
           in.
         </h2>
 
-        <form className="text-lg">
+        <form className="text-lg" onSubmit={handleSave}>
           <div className="grid grid-cols-1 gap-6 mt-4 ">
             <div>
               <label
                 className="text-gray-700 dark:text-gray-200"
-                htmlFor="username"
+                htmlFor="name"
               >
                 Name
               </label>
               <input
                 id="name"
+                name="name"
                 type="text"
                 className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-yellow-400 focus:ring-yellow-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
               />
@@ -35,23 +77,24 @@ const Home = () => {
             <div>
               <label
                 className="text-gray-700 dark:text-gray-200"
-                htmlFor="selectors"
+                htmlFor="sectors"
               >
                 Sectors
               </label>
               <select
-                id="selectors"
+                id="sectors"
+                name="sector"
                 multiple
                 size={5}
                 className="select select-warning w-full block px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-yellow-400 focus:ring-yellow-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
               >
-                {options?.map((option) => (
+                {sectors?.map((sector) => (
                   <option
-                    value={option._id}
-                    key={option._id}
+                    value={sector?.option}
+                    key={sector?._id}
                     className="text-lg my-1"
                   >
-                    {option?.option}
+                    {sector?.option}
                   </option>
                 ))}
               </select>
@@ -59,7 +102,11 @@ const Home = () => {
 
             <div>
               <label className="cursor-pointer label justify-start">
-                <input type="checkbox" className="checkbox checkbox-warning border-[3px]" />
+                <input
+                  name="agree"
+                  type="checkbox"
+                  className="checkbox checkbox-warning border-[3px]"
+                />
                 <span className=" text-gray-700 dark:text-gray-200 ml-3">
                   Agree to terms
                 </span>
